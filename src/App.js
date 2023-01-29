5e072b44dc01961d28f4c41946da70dfe81661dc
Marc-Anderson  Gilles <hornil6748@gmail.com>
2023-01-29 14:12:06 -0500

    lier node.js et react js avec react-express(API)

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+
+//DE LA VIDEO DE SIMPLILEARN CONNECT JS WITH NODE JS
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={apiResponse:""};
+  }
+  //DE LA VIDEO DE SIMPLILEARN CONNECT JS WITH NODE JS
+  callAPI(){
+    fetch("http://localhost:9000/testAPI")
+    .then(res => res.text())
+    .then(res => this.setState({apiResponse:res}))
+  }
+
+  componentWillMount(){
+    this.callAPI();
+  }
+
+render() {
   return (
     <div className="App">
       <h1>Mon premier projet avec React</h1>
+      <p>{this.state.apiResponse}</p>
     </div>
   );
+}
 }
 
 export default App;
