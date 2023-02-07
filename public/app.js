88f7393caec168369d8c70f680f46e5de29adce2
Marc-Anderson  Gilles <hornil6748@gmail.com>
2023-02-07 01:01:35 -0500

    firebase deploy & hosting

---
+++ public/app.js
@@ -0,0 +1,14 @@
+/////test video fire
+document.addEventListener("DOMContentLoaded", event => {
+    const app = firebase.app();
+    console.log(app)
+});
+function googleLogin(){
+    const provider = new firebase.auth.signInWithPopup(provider)
+
+    .then(result => {
+        const user = result.user;
+        //document.write('Hello ${user.displayName}');
+    })
+
+}
