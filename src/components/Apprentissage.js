42cdb677b1b1c4dc55caef542f404bd3ff0565e9
Marc-Anderson  Gilles <hornil6748@gmail.com>
2023-03-02 22:50:18 -0500

    debut page apprentissage

--- src/components/Apprentissage.js
+++ src/components/Apprentissage.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import "../sass/scss/apprentissages.scss";
 
+import reactJSapprentissage from '../assets/img/reactJSapprentissage.jpg';
+import javascriptXML from '../assets/img/javascriptXML.png';
+import firebaseImg from '../assets/img/firebase.png';
+
 function Apprentissage() {
   return (
     <div className='apprentissages'>
-        Apprentissage
+      
+        <div className='textBoxApprentissage1'>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+           It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+            essentially unchanged.</p>
+            <div><img className='imgIntro' src={reactJSapprentissage} alt="test2"/></div>
         </div>
+        <div className='textBoxApprentissage2'>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+           It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+            essentially unchanged.</p>
+            <div><img className='imgIntro' src={javascriptXML} alt="test2"/></div>
+        </div>
+        <div className='textBoxApprentissage3'>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+           It has survived not only five centuries, but also the leap into electronic typesetting, remaining
+            essentially unchanged.</p>
+            <div><img className='imgIntro' src={firebaseImg} alt="test2"/></div>
+        </div>
+        </div>
+        
   )
 }
 
